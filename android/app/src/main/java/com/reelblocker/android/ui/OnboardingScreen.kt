eda8721c58c87af13399ea562d11dcadf74dae73
Atharv Kulkarni <68152380+athukarad109@users.noreply.github.com>
2026-06-15 16:06:06 -0400

    Fix CI: add missing fillMaxSize import

--- android/app/src/main/java/com/reelblocker/android/ui/OnboardingScreen.kt
+++ android/app/src/main/java/com/reelblocker/android/ui/OnboardingScreen.kt
@@ -3,6 +3,7 @@ package com.reelblocker.android.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
